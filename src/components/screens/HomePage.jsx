301d972f72f89d83046fd46f00a0f366cbc03dc6
Lokesh Rajgadkar <lrajgadkar13@gmail.com>
2023-04-29 11:54:47 +0530

    removed unwanted import

--- src/components/screens/HomePage.jsx
+++ src/components/screens/HomePage.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './HomePage.css'
-// import Popular from './Popular'
-import TopAir from './TopAir'
 import requests from '../Requests'
 import Row from './Row'
 import Navbar from '../Navbar'
